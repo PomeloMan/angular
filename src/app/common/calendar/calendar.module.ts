3977e507e7c96baff0855d5cd86372867a839bd7
Chris Feng <fengchao.z@outlook.com>
2018-03-13 16:35:46 +0800

    Commit

--- src/app/common/calendar/calendar.module.ts
+++ src/app/common/calendar/calendar.module.ts
@@ -2,22 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatNativeDateModule } from '@angular/material/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 
-import { MatCalendar, MatCalendarBody, MatCalendarCell, MatYearView, ɵa31, MatMonthView } from '@angular/material/datepicker';
 import { CalendarComponent } from './calendar.component';
-import { format } from 'url';
 
 @NgModule({
     imports: [
         CommonModule,
         MatNativeDateModule,
-        MatToolbarModule,
-        MatGridListModule,
-        MatDividerModule,
         MatDatepickerModule
     ],
     declarations: [CalendarComponent],
